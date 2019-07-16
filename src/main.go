3460549bab54094b79a16e1fd9a2897e3df593ab
Timothy Onyiuke <timothy.onyiuke@andela.com>
2019-07-16 11:14:14 +0100

    Added template layouts and subtemplates

--- src/main.go
+++ src/main.go
@@ -1,32 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
-type myHandler struct {
-	greeting string
-}
-
-func (mh myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte(fmt.Sprintf("%v world", mh.greeting)))
-}
-
 func main() {
+	templates := populateTemplates()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		mh := myHandler{greeting: "Halos"}
-		w.Write([]byte(fmt.Sprintf("%v world!", mh.greeting)))
+		requestedFile := r.URL.Path[1:]
+		log.Println(requestedFile)
+		t := templates[requestedFile+".html"]
+		if t != nil {
+			err := t.Execute(w, nil)
+			if err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("Oh noes!! You got lost, my bad!"))
+		}
 	})
-	http.HandleFunc("/foo", func(w http.ResponseWriter, r *http.Request) {
-		mh := myHandler{greeting: "Halos"}
-		w.Write([]byte(fmt.Sprintf("%v Foo!", mh.greeting)))
-	})
-	redirect := http.RedirectHandler("/foo", 302)
-	http.HandleFunc("/back-to-foo", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Redirecting to foo")
-		redirect.ServeHTTP(w, r)
-	})
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
 	http.ListenAndServe(":8000", nil)
+}
+
+func populateTemplates() map[string]*template.Template {
+	result := make(map[string]*template.Template)
+	const basePath = "templates"
+	var layout = template.Must(template.ParseFiles(basePath + "/_layout.html"))
+	template.Must(layout.ParseFiles(basePath+"/_header.html", basePath+"/_footer.html"))
+	dir, err := os.Open(basePath + "/content")
+	if err != nil {
+		panic("Failed to open template directory: " + err.Error())
+	}
+	files, err := dir.Readdir(-1)
+	if err != nil {
+		panic("Failed to read template directory: " + err.Error())
+	}
+	for _, file := range files {
+		f, err := os.Open(basePath + "/content/" + file.Name())
+		if err != nil {
+			panic("Failed to open template '" + file.Name() + "'")
+		}
+		content, err := ioutil.ReadAll(f)
+		if err != nil {
+			panic("Failed to read file '" + file.Name() + "'")
+		}
+		f.Close()
+		tmpl := template.Must(layout.Clone())
+		_, err = tmpl.Parse(string(content))
+		if err != nil {
+			panic("Failed to parse template: '" + file.Name() + "'")
+		}
+		result[file.Name()] = tmpl
+	}
+	return result
 }
